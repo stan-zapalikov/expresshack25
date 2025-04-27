2d4fab51ee7b9d2ab5ca2efffcf98b58ecec4735
stan-zapalikov <stan@zapalikov.com>
2025-04-26 19:11:53 -0700

    Added placeholder themes

--- src/sandbox/code.js
+++ src/sandbox/code.js
@@ -4,6 +4,81 @@ import { editor } from "express-document-sdk";
 // Get the document sandbox runtime.
 const { runtime } = addOnSandboxSdk.instance;
 
+const themes = {
+    clarityMinimal: {
+        fontSize: {
+            title: 50,
+            heading: 30,
+            bullet: 20,
+        },
+        color: {
+            title: { red: 0, green: 0, blue: 0, alpha: 1 },         // Black
+            heading: { red: 0.2, green: 0.2, blue: 0.2, alpha: 1 }, // Dark grey
+            bullet: { red: 0.4, green: 0.4, blue: 0.4, alpha: 1 },  // Medium grey
+        },
+        spacing: {
+            titleTopMargin: 100,
+            bulletSpacing: 40,
+        },
+    },
+
+    midnightMinimal: {
+        fontSize: {
+            title: 50,
+            heading: 30,
+            bullet: 20,
+        },
+        color: {
+            title: { red: 1, green: 1, blue: 1, alpha: 1 },            // White
+            heading: { red: 0.8, green: 0.8, blue: 0.8, alpha: 1 },    // Light grey
+            bullet: { red: 0.6, green: 0.6, blue: 0.6, alpha: 1 },     // Dim grey
+        },
+        spacing: {
+            titleTopMargin: 100,
+            bulletSpacing: 40,
+        },
+        backgroundColor: { red: 0.1, green: 0.1, blue: 0.1, alpha: 1 }, // Almost black
+    },
+
+    executiveSlate: {
+        fontSize: {
+            title: 48,
+            heading: 32,
+            bullet: 22,
+        },
+        color: {
+            title: { red: 0.15, green: 0.15, blue: 0.2, alpha: 1 },    // Slate dark
+            heading: { red: 0.3, green: 0.3, blue: 0.4, alpha: 1 },    // Slate medium
+            bullet: { red: 0.5, green: 0.5, blue: 0.6, alpha: 1 },     // Slate light
+        },
+        spacing: {
+            titleTopMargin: 90,
+            bulletSpacing: 35,
+        },
+        backgroundColor: { red: 0.9, green: 0.92, blue: 0.95, alpha: 1 }, // Soft white
+    },
+
+    brightPop: {
+        fontSize: {
+            title: 60,
+            heading: 40,
+            bullet: 25,
+        },
+        color: {
+            title: { red: 1, green: 0.3, blue: 0.4, alpha: 1 },        // Bright red-pink
+            heading: { red: 0.2, green: 0.6, blue: 1, alpha: 1 },      // Sky blue
+            bullet: { red: 0.5, green: 0.8, blue: 0.3, alpha: 1 },     // Lime green
+        },
+        spacing: {
+            titleTopMargin: 120,
+            bulletSpacing: 50,
+        },
+        backgroundColor: { red: 1, green: 1, blue: 1, alpha: 1 }, // Pure white
+    },
+};
+
+
+
 function start() {
     // APIs to be exposed to the UI runtime
     // i.e., to the `index.html` file of this add-on.
